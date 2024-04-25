89a4dafb3b44787230e158f81bae074f74c76689
yinhao <1469786443@qq.com>
2024-04-25 08:14:31 +0800

    add

--- docs/assets/js/articles-detail.js
+++ docs/assets/js/articles-detail.js
@@ -4,7 +4,7 @@ const htmlFun = (src = "") => {
   return `
     <div class="img__preview">
       <img src="${src}" />
-      <span id="imgClose">X</span>
+      <span id="imgClose"><svg t="1714003776752" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4260" width="48" height="48"><path d="M1024 930.133333L930.133333 1024 512 605.866667 93.866667 1024 0 930.133333 418.133333 512 0 93.866667 93.866667 0 512 418.133333 930.133333 0 1024 93.866667 605.866667 512z" fill="#bfbfbf" p-id="4261"></path></svg></span>
     </div>
   `;
 };
